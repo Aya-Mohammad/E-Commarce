9ac06c34335d6ffd6877907eb73437f40598f3dc
eliasnadder <eliasn.learning@gmail.com>
2026-04-22 00:14:10 +0300

    Add update product request

---
+++ app/Http/Requests/System/Product/UpdateProductRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests\System\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => 'sometimes|required|string|unique:products,name,' . $this->route('id'),
+            'discraption' => 'sometimes|required|string',
+            'price' => 'sometimes|required|numeric',
+            'quantity' => 'sometimes|required|integer',
+            'store_id' => 'sometimes|required|exists:stores,id',
+            'image_path' => 'nullable|mimes:jpeg,png,jpg,gif',
+        ];
+    }
+}
