0ee1d0c5dd9aeec2d24c0de99d99cfe8af34e8fb
eliasnadder <eliasn.learning@gmail.com>
2026-04-22 00:16:22 +0300

    Fix bugs

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -10,12 +10,11 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'image_path' => $this->image_path,
+            'image_path' => $this->image?->image_path,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'phone' => $this->phone,
             'location' => $this->location,
-            'password' => $this->password, // يتم عرض كلمة المرور هنا
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
